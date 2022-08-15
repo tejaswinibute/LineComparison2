e1ba4245204e03cbd42ada6f01f26642c3728471
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 16:52:59 +0530

    [Tejaswini Bute] Add Lenghth Calculation

---
+++ LineComparison2/LineComparison2/LenghthCalculation.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison2
+{
+    public class LenghtCalculation
+    {
+        public static void LengthCalculation(int X1, int X2, int Y1, int Y2)
+        {
+            var distance = Math.Sqrt((Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2)));
+            Console.WriteLine("Length of a line is " + distance);
+        }
+    }
+}
---
+++ LineComparison2/LineComparison2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparison2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LenghthCalculation lenghth = new LenghthCalculation();
+            lenghth.Calculation();
+
+        }
+    }
+}
